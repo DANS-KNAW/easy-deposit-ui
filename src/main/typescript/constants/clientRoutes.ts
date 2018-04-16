d83a3ebde3956b741fcde2d8da24d0a3731c3749
lindar <linda.reijnhoudt@dans.knaw.nl>
2018-04-16 13:45:30 +0200

    Use 'deposit' as homeRoute

--- src/main/typescript/constants/clientRoutes.ts
+++ src/main/typescript/constants/clientRoutes.ts
@@ -15,8 +15,8 @@
  */
 import { DepositId } from "../model/Deposits"
 
-export const homeRoute = "/"
-export const registerRoute = "/register" // this route doesn't exist, but to be complete, I add it here as well
-export const loginRoute = "/login"
-export const depositFormRoute = (id: DepositId) => `/deposit-form/${id}`
-export const depositOverviewRoute = "/deposit-overview"
+export const homeRoute = "/deposit"
+export const registerRoute = `${homeRoute}/register` // this route doesn't exist, but to be complete, I add it here as well
+export const loginRoute = `${homeRoute}/login`
+export const depositFormRoute = (id: DepositId) => `${homeRoute}/deposit-form/${id}`
+export const depositOverviewRoute = `${homeRoute}/deposit-overview`
