302c826634149e29313ff866c0a49b704d7e5439
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-05-25 15:41:09 +0200

    abstracting the action creators

--- src/main/typescript/actions/dropdownActions.ts
+++ src/main/typescript/actions/dropdownActions.ts
@@ -3,15 +3,15 @@ import { ReduxAction } from "../lib/redux"
 import axios from "axios"
 import { DropdownListEntry } from "../model/DropdownLists"
 
-export const fetchAudienceData: () => ReduxAction<Promise<DropdownListEntry[]>> = () => ({
-    type: DropdownConstants.FETCH_AUDIENCE_DROPDOWN,
+const createFetchAction: (type: DropdownConstants, filename: string) => ReduxAction<Promise<DropdownListEntry[]>> = (type, filename) => ({
+    type: type,
     async payload() {
-        const response = await axios.get(require("../../resources/constants/audiences.json"))
-        return convertAudienceData(response.data)
+        const response = await axios.get(require(`../../resources/constants/${filename}`))
+        return convertDropdownData(response.data)
     }
 })
 
-const convertAudienceData: (data: any) => DropdownListEntry[] = data => {
+const convertDropdownData: (data: any) => DropdownListEntry[] = data => {
     return Object.keys(data)
         .map(key => {
             const obj = data[key]
@@ -24,7 +24,13 @@ const convertAudienceData: (data: any) => DropdownListEntry[] = data => {
         })
 }
 
-export const fetchAudienceDataFailed: (errorMessage: string) => ReduxAction<string> = errorMessage => ({
-    type: DropdownConstants.FETCH_AUDIENCE_DROPDOWN_FAILED,
+const createFailedAction: (type: DropdownConstants) => (errorMessage: string) => ReduxAction<string> = type => errorMessage => ({
+    type: type,
     payload: errorMessage,
 })
+
+export const fetchAudienceData: () => ReduxAction<Promise<DropdownListEntry[]>> = () =>
+    createFetchAction(DropdownConstants.FETCH_AUDIENCE_DROPDOWN, "audiences.json")
+
+export const fetchAudienceDataFailed: (errorMessage: string) => ReduxAction<string> =
+    createFailedAction(DropdownConstants.FETCH_AUDIENCE_DROPDOWN_FAILED)
