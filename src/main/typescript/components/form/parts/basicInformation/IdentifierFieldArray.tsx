dd2cdcde94442f1bb4817b9ab7842853d7243656
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-05-25 16:30:53 +0200

    identifier field array

---
+++ src/main/typescript/components/form/parts/basicInformation/IdentifierFieldArray.tsx
@@ -0,0 +1,31 @@
+import * as React from "react"
+import { connect } from "react-redux"
+import LoadDropdownData from "../../../../lib/formComponents/LoadDropdownData"
+import { FieldArrayProps } from "../../../../lib/formComponents/RepeatableField"
+import SchemedTextFieldArray from "../../../../lib/formComponents/SchemedTextFieldArray"
+import { SchemedValue } from "../../../../lib/metadata/Value"
+import { AppState } from "../../../../model/AppState"
+import { DropdownList } from "../../../../model/DropdownLists"
+
+interface IdentifierFieldArrayReduxProps {
+    identifiers: DropdownList
+}
+
+type IdentifierFieldArrayProps = FieldArrayProps<SchemedValue> & IdentifierFieldArrayReduxProps
+
+const IdentifierFieldArray = ({ identifiers: { state, list }, ...rest }: IdentifierFieldArrayProps) => (
+    <LoadDropdownData state={state}>
+        <SchemedTextFieldArray {...rest} withEmptyDefault schemeValues={
+            list.map(entry => ({
+                key: entry.key,
+                value: entry.displayValue,
+            }))
+        }/>
+    </LoadDropdownData>
+)
+
+const mapStateToProps = (state: AppState) => ({
+    identifiers: state.dropDowns.identifiers,
+})
+
+export default connect(mapStateToProps)(IdentifierFieldArray)
