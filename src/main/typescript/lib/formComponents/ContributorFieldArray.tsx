bd601c4fc9d47e0daebcf708dbc56f33decafeac
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-06-06 11:17:07 +0200

    split ContributorFieldArray into multiple internal components to support optional roles

--- src/main/typescript/lib/formComponents/ContributorFieldArray.tsx
+++ src/main/typescript/lib/formComponents/ContributorFieldArray.tsx
@@ -43,72 +43,136 @@ const ContributorIdArray = (idValues: DropdownListEntry[]) => (
     }
 )
 
+interface ContributorIdRowsProps {
+    idName: string
+    idValues: DropdownListEntry[]
+}
+
+const ContributorIdRows = ({ idName, idValues }: ContributorIdRowsProps) => (
+    <div className="container pl-0 pr-0">
+        <div className="row ml-0 mr-0">
+            <RepeatableField name={idName}
+                             label="Contributor Ids"
+                             empty={emptySchemedValue}
+                             fieldNames={[
+                                 (name: string) => `${name}.scheme`,
+                                 (name: string) => `${name}.value`,
+                             ]}
+                             component={ContributorIdArray(idValues)}/>
+        </div>
+    </div>
+)
+
+interface FirstRowProps {
+    titleName: string
+    initialsName: string
+    insertionsName: string
+    surnameName: string
+}
+
+const FirstRow = ({ titleName, initialsName, insertionsName, surnameName }: FirstRowProps) => (
+    <>
+        <div className="form-group col-md-3 mb-2">
+            <label>Titles</label>
+            <Field name={titleName}
+                   placeholder="(acadamic) title(s)"
+                   component={TextField}/>
+        </div>
+        <div className="form-group col-md-3 mb-2">
+            <label>Initials</label>
+            <Field name={initialsName}
+                   placeholder="initials"
+                   component={TextField}/>
+        </div>
+        <div className="form-group col-md-3 mb-2">
+            <label>Insertions</label>
+            <Field name={insertionsName}
+                   placeholder="insertions"
+                   component={TextField}/>
+        </div>
+        <div className="form-group col-md-3 mb-2">
+            <label>Surname</label>
+            <Field name={surnameName}
+                   placeholder="surname"
+                   component={TextField}/>
+        </div>
+    </>
+)
+
+interface OrganizationProps {
+    organizationName: string
+    onDelete: () => void
+    deleteDisabled: boolean
+}
+
+const Organization = ({ organizationName, onDelete, deleteDisabled }: OrganizationProps) => (
+    <>
+        <label>Organization</label>
+        <div className="input-group">
+            <Field name={organizationName}
+                   placeholder="organization"
+                   component={TextField}/>
+            <RemoveButton onClick={onDelete}
+                          disabled={deleteDisabled}/>
+        </div>
+    </>
+)
+
+interface SecondRowWithRoleProps {
+    roleName: string
+    roleValues: DropdownListEntry[]
+    organizationName: string
+    onDelete: () => void
+    deleteDisabled: boolean
+}
+
+const SecondRowWithRole = ({ roleName, roleValues, organizationName, onDelete, deleteDisabled }: SecondRowWithRoleProps) => (
+    <>
+        <div className="form-group col-md-4 mb-2">
+            <label>Role</label>
+            <Field name={roleName}
+                   choices={roleValues || []}
+                   withEmptyDefault
+                   emptyDefault="-no role-"
+                   component={DropdownFieldInput}/>
+        </div>
+        <div className="form-group col-md-8 mb-2">
+            <Organization organizationName={organizationName}
+                          onDelete={onDelete}
+                          deleteDisabled={deleteDisabled}/>
+        </div>
+    </>
+)
+
 interface ContributorFieldProps extends InnerComponentProps {
     idValues: DropdownListEntry[]
-    roleValues: DropdownListEntry[]
+    roleValues?: DropdownListEntry[]
 }
 
 const ContributorField = ({ names, onDelete, deleteDisabled, idValues, roleValues }: ContributorFieldProps) => (
     <div className="contributor mb-3">
-        <div className="container pl-0 pr-0">
-            <div className="row ml-0 mr-0">
-                <RepeatableField name={names[4]}
-                                 label="Contributor Ids"
-                                 empty={emptySchemedValue}
-                                 fieldNames={[
-                                     (name: string) => `${name}.scheme`,
-                                     (name: string) => `${name}.value`,
-                                 ]}
-                                 component={ContributorIdArray(idValues)}/>
-            </div>
+        <ContributorIdRows idName={names[4]}
+                           idValues={idValues}/>
+
+        <div className="form-row">
+            <FirstRow titleName={names[0]}
+                      initialsName={names[1]}
+                      insertionsName={names[2]}
+                      surnameName={names[3]}/>
         </div>
 
         <div className="form-row">
-            <div className="form-group col-md-3 mb-2">
-                <label>Titles</label>
-                <Field name={names[0]}
-                       placeholder="(acadamic) title(s)"
-                       component={TextField}/>
-            </div>
-            <div className="form-group col-md-3 mb-2">
-                <label>Initials</label>
-                <Field name={names[1]}
-                       placeholder="initials"
-                       component={TextField}/>
-            </div>
-            <div className="form-group col-md-3 mb-2">
-                <label>Insertions</label>
-                <Field name={names[2]}
-                       placeholder="insertions"
-                       component={TextField}/>
-            </div>
-            <div className="form-group col-md-3 mb-2">
-                <label>Surname</label>
-                <Field name={names[3]}
-                       placeholder="surname"
-                       component={TextField}/>
-            </div>
-        </div>
-
-        <div className="form-row">
-            <div className="form-group col-md-4 mb-2">
-                <label>Role</label>
-                <Field name={names[5]}
-                       choices={roleValues}
-                       withEmptyDefault
-                       emptyDefault="-no role-"
-                       component={DropdownFieldInput}/>
-            </div>
-            <div className="form-group col-md-8 mb-2">
-                <label>Organization</label>
-                <div className="input-group">
-                    <Field name={names[6]}
-                           placeholder="organization"
-                           component={TextField}/>
-                    <RemoveButton onClick={onDelete}
-                                  disabled={deleteDisabled}/>
-                </div>
-            </div>
+            {roleValues
+                ? <SecondRowWithRole roleName={names[5]}
+                                     roleValues={roleValues}
+                                     organizationName={names[6]}
+                                     onDelete={onDelete}
+                                     deleteDisabled={deleteDisabled}/>
+                : <div className="form-group col-md-12 mb-2">
+                    <Organization organizationName={names[6]}
+                                  onDelete={onDelete}
+                                  deleteDisabled={deleteDisabled}/>
+                </div>}
         </div>
     </div>
 )
