cc316df6c0969f7ecf1b2a60694a03e8d977755b
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-06-07 13:09:57 +0200

    tests for language (de)conversions

---
+++ src/test/typescript/lib/metadata/Language.spec.ts
@@ -0,0 +1,181 @@
+import { expect } from "chai"
+import "mocha"
+import {
+    languageOfDescriptionConverter,
+    languageOfDescriptionDeconverter,
+    languageOfFilesDeconverter,
+    languageOfFilesIsoDeconverter,
+    languagesOfFilesConverter,
+} from "../../../../main/typescript/lib/metadata/Language"
+import { DropdownListEntry } from "../../../../main/typescript/model/DropdownLists"
+
+describe("Language", () => {
+
+    const languages: DropdownListEntry[] = [
+        {
+            key: "nld",
+            value: "Dutch",
+            displayValue: "Dutch",
+        },
+        {
+            key: "deu",
+            value: "German",
+            displayValue: "German",
+        },
+    ]
+
+    describe("languageOfDescriptionConverter", () => {
+
+        it("should convert a valid description language object and extract the language", () => {
+            const input = {
+                scheme: "dcterms:ISO639-2",
+                key: "nld",
+                value: "Dutch",
+            }
+            const expected = "nld"
+            expect(languageOfDescriptionConverter(languages)(input)).to.eql(expected)
+        })
+
+        it("should fail when an invalid language is given", () => {
+            const input = {
+                scheme: "dcterms:ISO639-2",
+                key: "eng", // in principle valid, but doesn't appear in the listing above
+                value: "English",
+            }
+            expect(() => languageOfDescriptionConverter(languages)(input)).to
+                .throw("Error in metadata: no such language of description found: 'eng'")
+        })
+
+        it("should fail when the description language object has no scheme", () => {
+            const input = {
+                // no scheme
+                key: "nld",
+                value: "Dutch",
+            }
+            expect(() => languageOfDescriptionConverter(languages)(input)).to
+                .throw("Error in metadata: no such language scheme: 'undefined'")
+        })
+
+        it("should fail when the description language object as an invalid scheme", () => {
+            const input = {
+                scheme: "dcterms:invalid",
+                key: "nld",
+                value: "Dutch",
+            }
+            expect(() => languageOfDescriptionConverter(languages)(input)).to
+                .throw("Error in metadata: no such language scheme: 'dcterms:invalid'")
+        })
+    })
+
+    describe("languageOfDescriptionDeconverter", () => {
+
+        it("should convert a description language to the correct external model", () => {
+            const input = "nld"
+            const expected = {
+                scheme: "dcterms:ISO639-2",
+                key: "nld",
+                value: "Dutch",
+            }
+            expect(languageOfDescriptionDeconverter(languages)(input)).to.eql(expected)
+        })
+
+        it("should fail when an unknown language is given", () => {
+            const input = "eng" // in principle valid, but doesn't appear in the listing above
+            expect(() => languageOfDescriptionDeconverter(languages)(input)).to
+                .throw("Error in metadata: no valid language of description found for key 'eng'")
+        })
+    })
+
+    describe("languagesOfFilesConverter", () => {
+
+        it("should convert a valid sequence of file languages and split them correctly", () => {
+            const input = [
+                {
+                    scheme: "dcterms:ISO639-2",
+                    key: "deu",
+                    value: "German",
+                },
+                {
+                    scheme: "dcterms:ISO639-2",
+                    key: "nld",
+                    value: "Dutch",
+                },
+                {
+                    value: "Flakkees",
+                },
+                {
+                    value: "Goerees",
+                },
+            ]
+            const expected = [
+                ["deu", "nld"],
+                ["Flakkees", "Goerees"],
+            ]
+            expect(languagesOfFilesConverter(languages)(input)).to.eql(expected)
+        })
+
+        it("should fail when an unknown language is encountered containing an ISO tag", () => {
+            const input = [
+                { // in principle valid, but doesn't appear in the listing above
+                    scheme: "dcterms:ISO639-2",
+                    key: "eng",
+                    value: "English",
+                },
+            ]
+            expect(() => languagesOfFilesConverter(languages)(input)).to
+                .throw("Error in metadata: no such language of files found: 'eng'")
+        })
+
+        it("should fail when a file language is not properly formatted", () => {
+            const input = [
+                {
+                    invalid: "Foobar",
+                },
+            ]
+            expect(() => languagesOfFilesConverter(languages)(input)).to
+                .throw("Error in metadata: unrecognized language of files object: {\"invalid\":\"Foobar\"}")
+        })
+    })
+
+    describe("languageOfFilesIsoDeconverter", () => {
+
+        it("should convert a iso file language to the correct external model", () => {
+            const input = "deu"
+            const expected = {
+                scheme: "dcterms:ISO639-2",
+                key: "deu",
+                value: "German",
+            }
+            expect(languageOfFilesIsoDeconverter(languages)(input)).to.eql(expected)
+        })
+
+        it("should fail when the given language is not in the dropdown list", () => {
+            const input = "eng" // in principle valid, but doesn't appear in the listing above
+            expect(() => languageOfFilesIsoDeconverter(languages)(input)).to
+                .throw("Error in metadata: no valid language of files found for key 'eng'")
+        })
+
+        it("should return an empty object when given an empty string", () => {
+            const input = ""
+            const expected = {}
+            expect(languageOfFilesIsoDeconverter(languages)(input)).to.eql(expected)
+        })
+    })
+
+    describe("languageOfFilesDeconverter", () => {
+
+        it("should convert a file language to the correct external model", () => {
+            const input = "Flakkees"
+            const expected = {
+                value: "Flakkees"
+            }
+            expect(languageOfFilesDeconverter(input)).to.eql(expected)
+        })
+
+        it("should return an empty object when given an empty string", () => {
+            const input = ""
+            const expected = {}
+            expect(languageOfFilesDeconverter(input)).to.eql(expected)
+        })
+    })
+})
