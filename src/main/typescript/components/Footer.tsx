9beb2150dd4abd732ebddfa3aa5f9590a6115733
Vesa Akerman <vesa.akerman@dans.knaw.nl>
2018-06-19 14:43:28 +0200

    Footer refactored

--- src/main/typescript/components/Footer.tsx
+++ src/main/typescript/components/Footer.tsx
@@ -27,6 +27,143 @@ interface FooterState {
     apiUrl?: string
 }
 
+const Contact = () => (
+    <div className="col-6 col-sm-3 col-md-3 col-lg-2">
+        <ul className="contact">
+            <li>Anna van Saksenlaan 51</li>
+            <li>2593 HW Den Haag</li>
+            <li>
+                <a href="mailto:info@dans.knaw.nl">info@dans.knaw.nl</a>
+            </li>
+            <li>
+                <a title="View how to contact"
+                   href="http://dans.knaw.nl/en/contact"
+                   target="_blank">More ›››</a>
+            </li>
+        </ul>
+    </div>
+)
+
+const Social = () => (
+    <div className="col-6 col-sm-3 col-md-2 col-lg-2">
+        <ul className="social">
+            <li>
+                <a href="http://dans.knaw.nl/en/@@dans_mailchimp_subscribe" target="_blank">
+                    <i className="fas fa-envelope-square" aria-hidden="true"/> Newsletter
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="https://twitter.com/DANSKNAW">
+                    <i className="fab fa-twitter-square" aria-hidden="true"/> Twitter
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="https://www.youtube.com/user/DANSDataArchiving">
+                    <i className="fab fa-youtube-square" aria-hidden="true"/> YouTube
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="https://www.linkedin.com/company/dans">
+                    <i className="fab fa-linkedin" aria-hidden="true"/> LinkedIn
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+const Links = () => (
+    <div className="col-12 col-sm-3 col-md-2 col-lg-2">
+        <ul>
+            <li>
+                <a title="View the disclaimer"
+                   href="http://dans.knaw.nl/en/about/services/data-archiving-and-reuse/easy/disclaimer-easy"
+                   target="_blank">Disclaimer</a>
+            </li>
+            <li>
+                <a title="View the legal information"
+                   href="http://www.dans.knaw.nl/nl/over/organisatie-beleid/juridische-informatie"
+                   target="_blank">Legal information</a>
+            </li>
+            <li>
+                <a title="View the property right statement"
+                   href="http://www.dans.knaw.nl/en/about/organisation-and-policy/legal-information/property-rights-statement"
+                   target="_blank">Property rights statement</a>
+            </li>
+            <li>
+                <a href="http://dans.knaw.nl/en/search/about-reusing-data" target="_blank"
+                   title="How to cite data">How to cite data</a>
+            </li>
+        </ul>
+    </div>
+)
+
+const Certification = () => (
+    <div className="col-12 col-sm-4 col-md-4 col-lg-3">
+        <div className="easy-Certificering-link">
+            <a href="http://www.dans.knaw.nl/nl/over/organisatie-beleid/Certificering"
+               target="_blank"
+               title="Certificering">
+                <img src={cts}/>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <img src={nestor}/>
+            </a>
+        </div>
+    </div>
+)
+
+const AboutDans = () => (
+    <div className="col-lg-3">
+        <div className="overdans">
+            <p>DANS is an institute of KNAW and NWO</p>
+            <p>
+                <a href="http://www.knaw.nl/en" target="_blank">
+                    <img height="55px" src={knaw}/>
+                </a>
+                <a href="http://www.nwo.nl/en" target="_blank">
+                    <img height="55px" src={nwo}/>
+                </a>
+            </p>
+            <p className="driven-by-data">Driven by data</p>
+        </div>
+    </div>
+)
+
+const FooterTitle = () => (
+    <div className="row">
+        <div className="col-12 col-sm-12 col-md-3 col-lg-2">
+            <div className="h1">Contact</div>
+        </div>
+    </div>
+)
+
+const FooterContents = () => (
+    <div className="row">
+        <Contact/>
+        <Social/>
+        <Links/>
+        <Certification/>
+        <AboutDans/>
+    </div>
+)
+
+interface FooterBuildInfoProps {
+    apiUrl?: string
+}
+
+const FooterBuildInfo = ({apiUrl}: FooterBuildInfoProps) => (
+    <div className="row">
+        <div className="col-12">
+            <div className="version">
+                    <span>
+                        <div>Version: <span>{projectVersion}</span></div>
+                        <div>Build date: <span>{buildDate}</span></div>
+                        {inDevelopmentMode && apiUrl && <div>API base url: <span>{apiUrl}</span></div>}
+                    </span>
+            </div>
+        </div>
+    </div>
+)
+
 class Footer extends Component<{}, FooterState> {
     constructor(props: {}) {
         super(props)
@@ -41,111 +178,9 @@ class Footer extends Component<{}, FooterState> {
     render() {
         return (
             <footer className="container-fluid">
-                <div className="row">
-                    <div className="col-12 col-sm-12 col-md-3 col-lg-2">
-                        <div className="h1">Contact</div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-6 col-sm-3 col-md-3 col-lg-2">
-                        <ul className="contact">
-                            <li>Anna van Saksenlaan 51</li>
-                            <li>2593 HW Den Haag</li>
-                            <li>
-                                <a href="mailto:info@dans.knaw.nl">info@dans.knaw.nl</a>
-                            </li>
-                            <li>
-                                <a title="View how to contact"
-                                   href="http://dans.knaw.nl/en/contact"
-                                   target="_blank">More ›››</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="col-6 col-sm-3 col-md-2 col-lg-2">
-                        <ul className="social">
-                            <li>
-                                <a href="http://dans.knaw.nl/en/@@dans_mailchimp_subscribe" target="_blank">
-                                    <i className="fas fa-envelope-square" aria-hidden="true"/> Newsletter
-                                </a>
-                            </li>
-                            <li>
-                                <a target="_blank" href="https://twitter.com/DANSKNAW">
-                                    <i className="fab fa-twitter-square" aria-hidden="true"/> Twitter
-                                </a>
-                            </li>
-                            <li>
-                                <a target="_blank" href="https://www.youtube.com/user/DANSDataArchiving">
-                                    <i className="fab fa-youtube-square" aria-hidden="true"/> YouTube
-                                </a>
-                            </li>
-                            <li>
-                                <a target="_blank" href="https://www.linkedin.com/company/dans">
-                                    <i className="fab fa-linkedin" aria-hidden="true"/> LinkedIn
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="col-12 col-sm-3 col-md-2 col-lg-2">
-                        <ul>
-                            <li>
-                                <a title="View the disclaimer"
-                                   href="http://dans.knaw.nl/en/about/services/data-archiving-and-reuse/easy/disclaimer-easy"
-                                   target="_blank">Disclaimer</a>
-                            </li>
-                            <li>
-                                <a title="View the legal information"
-                                   href="http://www.dans.knaw.nl/nl/over/organisatie-beleid/juridische-informatie"
-                                   target="_blank">Legal information</a>
-                            </li>
-                            <li>
-                                <a title="View the property right statement"
-                                   href="http://www.dans.knaw.nl/en/about/organisation-and-policy/legal-information/property-rights-statement"
-                                   target="_blank">Property rights statement</a>
-                            </li>
-                            <li>
-                                <a href="http://dans.knaw.nl/en/search/about-reusing-data" target="_blank"
-                                   title="How to cite data">How to cite data</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="col-12 col-sm-4 col-md-4 col-lg-3">
-                        <div className="easy-Certificering-link">
-                            <a href="http://www.dans.knaw.nl/nl/over/organisatie-beleid/Certificering"
-                               target="_blank"
-                               title="Certificering">
-                                <img src={cts}/>
-                                &nbsp;&nbsp;&nbsp;&nbsp;
-                                <img src={nestor}/>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="col-lg-3">
-                        <div className="overdans">
-                            <p>DANS is an institute of KNAW and NWO</p>
-                            <p>
-                                <a href="http://www.knaw.nl/en" target="_blank">
-                                    <img height="55px" src={knaw}/>
-                                </a>
-                                <a href="http://www.nwo.nl/en" target="_blank">
-                                    <img height="55px" src={nwo}/>
-                                </a>
-                            </p>
-                            <p className="driven-by-data">Driven by data</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="row">
-                    <div className="col-12">
-                        <div className="version">
-                    <span>
-                        <div>Version: <span>{projectVersion}</span></div>
-                        <div>Build date: <span>{buildDate}</span></div>
-                        {inDevelopmentMode && <div>API base url: <span>{this.state.apiUrl}</span></div>}
-                    </span>
-                        </div>
-                    </div>
-                </div>
+                <FooterTitle/>
+                <FooterContents/>
+                <FooterBuildInfo apiUrl={this.state.apiUrl}/>
             </footer>
         )
     }
