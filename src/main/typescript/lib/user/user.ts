5857d35b99888395a876d4bebf64d2ab656d1717
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-08-14 15:31:32 +0200

    add default arguments in userConverter

--- src/main/typescript/lib/user/user.ts
+++ src/main/typescript/lib/user/user.ts
@@ -15,13 +15,14 @@
  */
 import { UserDetails } from "../../model/UserDetails"
 
+// TODO add unit tests
 export const userConverter: (input: any) => UserDetails = input => {
     return {
-        username: input.username,
-        firstName: input.firstName,
-        prefix: input.prefix,
-        lastName: input.lastName,
-        groups: input.groups,
-        displayName: `${input.firstName} ${input.prefix ? input.prefix+" " : ""}${input.lastName}`,
+        username: input.username || "",
+        firstName: input.firstName || "",
+        prefix: input.prefix || "",
+        lastName: input.lastName || "",
+        groups: input.groups || [],
+        displayName: `${input.firstName} ${input.prefix ? input.prefix + " " : ""}${input.lastName}`,
     }
 }
