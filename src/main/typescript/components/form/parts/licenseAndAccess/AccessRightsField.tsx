1e71305ad49db1997d24bb1fd1dc79b870a92472
Joke Pol <joke.pol@dans.knaw.nl>
2018-09-20 15:53:04 +0200

    do not display group access

--- src/main/typescript/components/form/parts/licenseAndAccess/AccessRightsField.tsx
+++ src/main/typescript/components/form/parts/licenseAndAccess/AccessRightsField.tsx
@@ -30,7 +30,6 @@ interface AccessRightsFieldProps {
 }
 
 const AccessRightsField = ({ userGroups, input, meta, label }: FieldProps & AccessRightsFieldProps) => {
-    const withUserGroups = userGroups.length > 0
 
     const choices = [
         {
@@ -42,11 +41,6 @@ const AccessRightsField = ({ userGroups, input, meta, label }: FieldProps & Acce
             value: "Restricted Access",
         },
     ]
-    if (withUserGroups)
-        choices.push({
-            title: AccessRightValue.GROUP_ACCESS,
-            value: "Group Access",
-        })
 
     return (
         <div className="form-row accessrights-field">
@@ -56,27 +50,10 @@ const AccessRightsField = ({ userGroups, input, meta, label }: FieldProps & Acce
                        choices={choices}
                        component={RadioChoicesInput}/>
             </div>
-            {withUserGroups && <div className="col col-md-4 group-field">
-                <Field name={`${input.name}.group`}
-                       choices={userGroups}
-                       withEmptyDefault={userGroups.length != 1}
-                       emptyDefault="Choose a group..."
-                       disabled={input.value.category !== AccessRightValue.GROUP_ACCESS}
-                       component={DropdownFieldInput}/>
-            </div>}
         </div>
     )
 }
 
-const mapStateToProps = (state: AppState) => ({
-    userGroups: state.user.groups.map(group => ({
-        key: group,
-        value: group,
-        displayValue: group,
-    })),
-})
-
 export default compose(
-    asField,
-    connect(mapStateToProps),
+    asField
 )(AccessRightsField)
