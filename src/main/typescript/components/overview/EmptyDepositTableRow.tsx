7197277941342cd9d6d16380a0d2c54ae292c4f1
lindareijnhoudt <linda.reijnhoudt@dans.knaw.nl>
2018-11-28 14:20:18 +0100

    Update src/main/typescript/components/overview/EmptyDepositTableRow.tsx
    Co-Authored-By: rvanheest <rvanheest@users.noreply.github.com>

--- src/main/typescript/components/overview/EmptyDepositTableRow.tsx
+++ src/main/typescript/components/overview/EmptyDepositTableRow.tsx
@@ -17,7 +17,7 @@ import * as React from "react"
 
 const EmptyDepositTableRow = () => (
     <tr className="row ml-0 mr-0">
-        <td className="col col-12" scope="row" colSpan={5}>No deposits found</td>
+        <td className="col col-12" scope="row" colSpan={5}>No deposits yet</td>
     </tr>
 )
 
