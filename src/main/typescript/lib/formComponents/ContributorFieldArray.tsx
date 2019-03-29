e4e59ed4922323666c771ee250513644bec00b2c
Richard van Heest <richard.van.heest@dans.knaw.nl>
2019-03-29 14:52:33 +0100

    remove a log statement

--- src/main/typescript/lib/formComponents/ContributorFieldArray.tsx
+++ src/main/typescript/lib/formComponents/ContributorFieldArray.tsx
@@ -33,10 +33,8 @@ function getPlaceholder(fields: FieldArrayFieldsProps<SchemedValue>, index: numb
         const scheme = contributorId.scheme
         if (scheme) {
             const contributorEntry = contributorIds.find(({ key }) => key == scheme)
-            if (contributorEntry) {
-                console.log(`placeholder: ${contributorEntry.placeholder}`)
+            if (contributorEntry)
                 return contributorEntry.placeholder
-            }
         }
     }
     return "ID"
