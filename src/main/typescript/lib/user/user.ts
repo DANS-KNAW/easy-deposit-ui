3eff827086b15fb59c7afd1f55d28614e0e88e5c
Jo Pol <jo-pol@users.noreply.github.com>
2019-08-15 15:31:38 +0200

    new field for user from api: displayName

--- src/main/typescript/lib/user/user.ts
+++ src/main/typescript/lib/user/user.ts
@@ -19,13 +19,14 @@ export const userConverter: (input: any) => UserDetails = input => {
     const firstName = input.firstName ? `${input.firstName} ` : ""
     const prefix = input.prefix ? `${input.prefix} ` : ""
     const lastName = input.lastName || ""
-    const displayName = `${firstName}${prefix}${lastName}`
+    const displayName = input.displayName || ""
+    const easyDisplayName = input.displayName || ""
 
     return {
         username: input.username || "",
         firstName: input.firstName || "",
         prefix: input.prefix || "",
         lastName: input.lastName || "",
-        displayName: displayName,
+        displayName: input.easyDisplayName || "",
     }
 }
