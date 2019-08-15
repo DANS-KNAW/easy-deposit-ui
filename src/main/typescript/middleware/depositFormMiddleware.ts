90e8108f06c101836b629a71c1f3bb838d99ca78
Richard van Heest <richard.van.heest@dans.knaw.nl>
2019-08-15 13:34:42 +0200

    refactoring of if-else statements

--- src/main/typescript/middleware/depositFormMiddleware.ts
+++ src/main/typescript/middleware/depositFormMiddleware.ts
@@ -59,10 +59,7 @@ const submitReroute: Middleware = () => (next: Dispatch) => action => {
 }
 
 const replaceContributorIdFieldValue: Middleware = ({ dispatch, getState }: MiddlewareAPI) => (next: Dispatch) => action => {
-    if (action.type === actionTypes.CHANGE
-        && action.meta
-        && action.meta.field
-        && action.payload) {
+    if (action.type === actionTypes.CHANGE && action.meta && action.meta.field && action.payload) {
         const state: AppState = getState()
         const formData = getFormValues(depositFormName)(state)
         if (action.meta.field.match(/^contributors\[\d+].ids\[\d+].scheme$/)) {
@@ -76,28 +73,23 @@ const replaceContributorIdFieldValue: Middleware = ({ dispatch, getState }: Midd
                     // replacement of the value itself happens on the pass-through of this new action through this method
                     dispatch(change(depositFormName, action.meta.field.replace(".scheme", ".value"), value))
             }
+            return
         }
         else if (action.meta.field.match(/^contributors\[\d+].ids\[\d+].value$/)) {
             const scheme = get(formData, action.meta.field.replace(".value", ".scheme"))
-
             if (scheme) {
                 const contributorEntry = state.dropDowns.contributorIds.list.find(({ key }: DropdownListEntry) => key === scheme)
-                if (contributorEntry && contributorEntry.replace)
+                if (contributorEntry && contributorEntry.replace) {
                     next({
                         ...action,
                         payload: contributorEntry.replace.reduce((v, cfg) => v.replace(cfg.from, cfg.to), action.payload),
                     })
-                else
-                    next(action)
+                    return
+                }
             }
-            else
-                next(action)
         }
-        else
-            next(action)
     }
-    else
-        next(action)
+    next(action)
 }
 
 export const depositFormMiddleware: Middleware[] = [
