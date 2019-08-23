4b15f29a7db4bae6bd7f482549c8ad8b73d82b60
Richard van Heest <richard.van.heest@dans.knaw.nl>
2019-08-23 13:21:47 +0200

    Provide link to deposit overview when the deposit is not found

--- src/main/typescript/components/form/DepositNotFound.tsx
+++ src/main/typescript/components/form/DepositNotFound.tsx
@@ -1,10 +1,12 @@
 import * as React from "react"
+import { Link } from "react-router-dom"
+import { depositOverviewRoute } from "../../constants/clientRoutes"
 
 const DepositNotFound = () => (
-    <>
-        <p>This deposit could not be found.</p>
-        {/* TODO button to go back to the deposit overview */}
-    </>
+    <p>
+        This deposit could not be found.
+        Please find your deposits <Link to={depositOverviewRoute}>here</Link>.
+    </p>
 )
 
 export default DepositNotFound
