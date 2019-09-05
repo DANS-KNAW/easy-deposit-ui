69defa8a1b17080f9881c9df424ecf39cf3c0d4a
Richard van Heest <richard.van.heest@dans.knaw.nl>
2019-09-05 15:05:40 +0200

    Info boxes for not accessible deposits (submitted, in review, archived)

--- src/main/typescript/components/form/DepositNotAccessible.tsx
+++ src/main/typescript/components/form/DepositNotAccessible.tsx
@@ -16,40 +16,32 @@
 import * as React from "react"
 import { DepositState } from "../../model/DepositState"
 import { DepositStateLabel } from "../../model/Deposits"
-import { Link } from "react-router-dom"
-import { depositOverviewRoute } from "../../constants/clientRoutes"
 import { DepositFormMetadata } from "./parts"
-
-function introLine(label: DepositStateLabel, metadata?: DepositFormMetadata) {
-    const title: string | undefined = metadata && metadata.titles && metadata.titles.length > 0 ? metadata.titles[0] : undefined
-    const doi = metadata && metadata.doi
-
-    const start = title && doi
-        ? <span>The deposit '<i>{title}</i>' (DOI {doi})</span>
-        : <span>Your deposit</span>
-    switch (label) {
-        case DepositStateLabel.SUBMITTED:
-            return <p>{start} has been submitted.</p>
-        case DepositStateLabel.ARCHIVED:
-            return <p>{start} has already been published.</p>
-        case DepositStateLabel.IN_PROGRESS:
-        default:
-            return <p>{start} is in review.</p>
-    }
-}
+import { Info } from "../Errors"
 
 interface DepositUnavailableProps {
     depositState: DepositState
     metadata?: DepositFormMetadata
 }
 
-const DepositNotAccessible = ({ depositState, metadata }: DepositUnavailableProps) => (
-    <>
-        {introLine(depositState.label, metadata)}
-        {depositState.description &&
-        <p style={{ whiteSpace: "pre-wrap" }} dangerouslySetInnerHTML={{ __html: depositState.description }}/>}
-        <p>Please find your deposits <Link to={depositOverviewRoute}>here</Link>.</p>
-    </>
-)
+const DepositNotAccessible = ({ depositState, metadata }: DepositUnavailableProps) => {
+    const title: string | undefined = metadata && metadata.titles && metadata.titles.length > 0 ? metadata.titles[0] : undefined
+    const doi = metadata && metadata.doi
+
+    const start = title && doi
+        ? <span>The deposit '<i>{title}</i>' (DOI {doi})</span>
+        : <span>Your deposit</span>
+    const end = depositState.description &&
+        <span style={{ whiteSpace: "pre-wrap" }} dangerouslySetInnerHTML={{ __html: depositState.description }}/>
+    switch (depositState.label) {
+        case DepositStateLabel.SUBMITTED:
+            return <Info>{start} has been submitted. {end}</Info>
+        case DepositStateLabel.ARCHIVED:
+            return <Info>{start} has already been published. {end}</Info>
+        case DepositStateLabel.IN_PROGRESS:
+        default:
+            return <Info>{start} is in review. {end}</Info>
+    }
+}
 
 export default DepositNotAccessible
