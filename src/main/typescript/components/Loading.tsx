973d340271cc6bd16b026dcd4071bb38ed775101
Richard van Heest <richard.van.heest@dans.knaw.nl>
2019-09-10 23:55:40 +0200

    loading spinner

---
+++ src/main/typescript/components/Loading.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+import "../../resources/css/loading.css"
+
+const Loading = () => (
+    <div className="loading-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+)
+
+export default Loading
