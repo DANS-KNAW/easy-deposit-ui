6025460bbfe99c391139dd108c0c2849ef86c6f3
Richard van Heest <richard.van.heest@dans.knaw.nl>
2020-02-28 21:40:39 +0100

    undo accidental commit

--- src/main/typescript/store.ts
+++ src/main/typescript/store.ts
@@ -29,8 +29,8 @@ import { inDevelopmentMode } from "./lib/config"
 // import { AppState } from "./model/AppState"
 // const predicate = (state: AppState, action: Action) => !action.type.startsWith('@@redux-form/CHANGE')
 
-const predicate = () => true // if you want to see all actions
-// const predicate = () => false // if you want to see no actions
+// const predicate = () => true // if you want to see all actions
+const predicate = () => false // if you want to see no actions
 
 export const newStore = () => {
     if (inDevelopmentMode) {
